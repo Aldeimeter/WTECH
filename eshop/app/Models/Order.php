532323dc4e554862c36de6a54035d3a5f0edd7a8
rykktiadLoans <datokar1705@gmail.com>
2024-05-10 14:37:35 +0200

    added models for buying

--- eshop/app/Models/Order.php
+++ eshop/app/Models/Order.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Order extends Model
 {
     use HasFactory;
+
+    protected $guarded = ["id"];
+    protected $fillable = ["userid", "address", "payment", "delivery", "date"];
+
+    public function users(): HasOne {
+        return $this->hasOne(User::class, "users");
+    }
+
+    public function orderitems(): HasMany {
+        return $this->hasMany(OrderItems::class, "order_items");
+    }
 }
--- eshop/app/Models/OrderItems.php
+++ eshop/app/Models/OrderItems.php
@@ -4,10 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class OrderItems extends Model
 {
     use HasFactory;
     protected $guarded = ["id"];
-    protected $fillable = ["bookid", "userid", "amount"];
+    protected $fillable = ["bookid", "orderid", "amount"];
+
+    public function books(): HasOne {
+        return $this->hasOne(Book::class, "books");
+    }
+
+    public function orders(): HasOne {
+        return $this->hasOne(Order::class, "orders");
+    }
 }
